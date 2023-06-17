8add5015ec34c6eaee3f0d7c3d6b87835ce191fd
noah0504789 <noah0504@naver.com>
2023-06-17 22:43:59 +0900

    [test/email] getToken()

---
+++ src/test/java/com/koliving/api/token/ConfirmationTokenServiceTest.java
@@ -0,0 +1,103 @@
+package com.koliving.api.token;
+
+import com.koliving.api.clock.IClock;
+import com.koliving.api.email.IEmailService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ConfirmationTokenServiceTest {
+
+    @Mock
+    ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Mock
+    IEmailService emailService;
+
+    @Mock
+    IClock clock;
+
+    ConfirmationTokenService confirmationTokenService;
+
+    String origin = "http://localhost:8080";
+    String currentVersion = "v1";
+    long validityPeriod = 30;
+
+    @BeforeEach
+    public void setUp() {
+        confirmationTokenService = new ConfirmationTokenService(
+                confirmationTokenRepository,
+                emailService,
+                clock,
+                origin,
+                currentVersion,
+                validityPeriod
+        );
+    }
+
+    @Test
+    @DisplayName("getToken() : token 값에 따른 행이 존재함")
+    void getToken_success() {
+        String testMail = "from@test.com";
+
+        ConfirmationToken token = ConfirmationToken.builder()
+                .email(testMail)
+                .build();
+        String tokenValue = token.getToken();
+
+        when(confirmationTokenRepository.findByToken(tokenValue)).thenReturn(Optional.of(token));
+
+        Optional<ConfirmationToken> result = confirmationTokenService.getToken(token.getToken());
+
+        assertTrue(result.isPresent());
+        assertEquals(tokenValue, result.get().getToken());
+
+        verify(confirmationTokenRepository, times(1)).findByToken(tokenValue);
+    }
+
+    @Test
+    @DisplayName("getToken() : token 값에 따른 행이 없음")
+    void getToken_failure() {
+
+        // 생성자 함수를 통해 token 값이 UUID.randomUUID().toString()의 응답값으로 할당됨
+        // 문자열만 구성된 token 값은 존재하지 않음
+        String nonExistentTokenValue = "invalid token value";
+        String tokenValue = nonExistentTokenValue;
+
+        when(confirmationTokenRepository.findByToken(tokenValue)).thenReturn(Optional.empty());
+
+        Optional<ConfirmationToken> result = confirmationTokenService.getToken(tokenValue);
+
+        assertTrue(result.isEmpty());
+
+        verify(confirmationTokenRepository, times(1)).findByToken(tokenValue);
+    }
+
+    @Test
+    void createToken() {
+    }
+
+    @Test
+    void saveToken() {
+    }
+
+    @Test
+    void sendEmail() {
+    }
+
+    @Test
+    void authenticateToken() {
+    }
+}

e72463a4857cd705a094242747fff3a059ca81f3
noah0504789 <noah0504@naver.com>
2023-06-18 21:02:38 +0900

    [test/email] saveToken()

--- src/test/java/com/koliving/api/token/ConfirmationTokenServiceTest.java
+++ src/test/java/com/koliving/api/token/ConfirmationTokenServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -100,7 +101,19 @@ class ConfirmationTokenServiceTest {
     // 컨트롤러 단에서 Java Bean Validation 기술을 사용한 유효성 검사를 수행하므로 검증 로직이 없음
 
     @Test
-    void saveToken() {
+    @DisplayName("saveToken() : crudRepository save api 정상 호출")
+    void saveToken_success() {
+        String testMail = "new@test.com";
+        ConfirmationToken token = ConfirmationToken.builder()
+                .email(testMail).build();
+
+        when(confirmationTokenRepository.save(any(ConfirmationToken.class))).thenReturn(token);
+
+        ConfirmationToken result = confirmationTokenService.saveToken(token);
+
+        assertNotNull(result);
+        assertEquals(result, token);
+        verify(confirmationTokenRepository, times(1)).save(token);
     }
 
     @Test

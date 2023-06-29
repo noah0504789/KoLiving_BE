3f28d101d2464ba0c735c76b882e805e73c00ac7
noah0504789 <noah0504@naver.com>
2023-06-29 22:13:33 +0900

    [feat/jwt] IJwtService

--- src/main/java/com/koliving/api/token/IJwtService.java
+++ src/main/java/com/koliving/api/token/IJwtService.java
@@ -2,16 +2,23 @@ package com.koliving.api.token;
 
 import org.springframework.security.core.Authentication;
 
+import java.util.Date;
+
 public interface IJwtService {
+
+    Date extractExpirationDate(String token);
 
     String extractEmail(String token);
 
     Authentication getAuthentication(String accessToken);
+
+    boolean isBlackList(String accessToken);
+
+    void registerBlackList(String accessToken, Date expirationDate);
 
     boolean isRefreshTokenPresent(String email);
 
     void deleteRefreshToken(String email);
 
     String saveRefreshToken(String email, String newRefreshTokenValue);
-
 }

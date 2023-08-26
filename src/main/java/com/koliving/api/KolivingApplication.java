bf2a997e0cd64b7e3dcc536a34764a80d9437b91
noah0504789 <noah0504@naver.com>
2023-08-26 21:12:33 +0900

    [config/db] set jvm timezone : UTC

--- src/main/java/com/koliving/api/KolivingApplication.java
+++ src/main/java/com/koliving/api/KolivingApplication.java
@@ -1,5 +1,6 @@
 package com.koliving.api;
 
+<<<<<<< HEAD
 import com.koliving.api.room.domain.Furnishing;
 import com.koliving.api.room.domain.FurnishingType;
 import com.koliving.api.room.infra.FurnishingRepository;
@@ -7,11 +8,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.boot.CommandLineRunner;
+=======
+import jakarta.annotation.PostConstruct;
+>>>>>>> 1d10d8b... feat: set jvm timezone to utc
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.koliving.api.properties")
@@ -21,6 +27,7 @@ public class KolivingApplication {
         SpringApplication.run(KolivingApplication.class, args);
     }
 
+<<<<<<< HEAD
     @Profile("default")
     @Bean
     CommandLineRunner commandLineRunner(FurnishingRepository furnishingRepository) {
@@ -35,5 +42,10 @@ public class KolivingApplication {
             .collect(Collectors.toList());
 
         furnishingRepository.saveAll(furnishings);
+=======
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+>>>>>>> 1d10d8b... feat: set jvm timezone to utc
     }
 }
